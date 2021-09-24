46c0aa0717fda78de993c90a2df20bd7a4e8a851
Zack Adams <imzackadams@protonmail.com>
2021-09-24 05:16:08 -0400

    modify CR comments

--- DB.sql
+++ DB.sql
@@ -9,27 +9,25 @@ CREATE TABLE civilian (
 	civilian_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     age INT NOT NULL,
     ethnicity VARCHAR(20) NOT NULL, -- maybe some consistency issue here?
-    gender ENUM('male', 'female', 'other', 'prefer not to say') NOT NULL
+    gender INT NOT NULL
 	
 );
 
 CREATE TABLE citation (
 	-- if citation did not occur, no data inserted?
 	citation_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    citation_classification ENUM('felony', 'misdemeanou', 'infraction'),
-    mandotory_court BOOLEAN NOT NULL,
-	citation_date DATE NOT NULL,
-    citation_time TIME NOT NULL,
+    citation_classification INT NOT NULL,
+    mandatory_court BOOLEAN NOT NULL,
+	citation_date DATETIME NOT NULL,
     fine_ammount INT NOT NULL
     -- video, how to? text to link?
 	
-   
    );
    
 CREATE TABLE eventcontext (
 	event_id INT AUTO_INCREMENT PRIMARY KEY,
 	search_conducted BOOLEAN NOT NULL,
-    probable_cause TEXT,
+    stated_probable_cause TEXT,
     destination TEXT,
     civilian_id INT,
     citation_id INT,
