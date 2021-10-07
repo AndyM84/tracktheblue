a55038df7519e940022261a6f5ddca29267f93d5
imzackadams <73737400+imzackadams@users.noreply.github.com>
2021-10-06 20:35:20 -0400

    create ddl (#1)
    * create ddl
    
    * modify CR comments
    
    * modify DDL for change request
    
    * update DDL indentation

---
+++ DB.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS `ttb`;
+
+CREATE DATABASE `ttb`;
+
+USE `ttb`;
+
+
+CREATE TABLE `civilian` (
+    `civilian_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `age` INT NOT NULL,
+    `ethnicity` VARCHAR(20) NOT NULL, -- maybe some consistency issue here?
+    `gender` INT NOT NULL
+	
+);
+
+CREATE TABLE `citation` (
+     -- if citation did not occur, no data inserted?
+    `citation_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `citation_classification` INT NOT NULL,
+    `mandatory_court` BOOLEAN NOT NULL,
+    `citation_date` DATETIME NOT NULL,
+    `fine_ammount` INT NOT NULL,
+    `video` TEXT
+	
+   );
+   
+CREATE TABLE eventcontext (
+    `event_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `search_conducted` BOOLEAN NOT NULL,
+    `stated_probable_cause` TEXT,
+    `destination` TEXT,
+    `civilian_id` INT,
+    `citation_id` INT,
+    FOREIGN KEY (civilian_id) REFERENCES civilian (civilian_id),
+    FOREIGN KEY (citation_id) REFERENCES citation (citation_id)
+    
+   );
+   
+   
