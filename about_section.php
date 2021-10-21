a296b4557b526a72598cb519854800363b451b0d
Tomovasky <31634128+Tomovasky@users.noreply.github.com>
2021-10-20 17:13:50 -0700

    adding plates template and seperating PHP and HTML

---
+++ about_section.php
@@ -0,0 +1,8 @@
+<?php $this->layout('template',['title'=>'about_section'])?>
+
+<div id="about_the_project">
+            <p>Isum Copy... sdfskdhf;kjhsdjfh</p>    
+            <p>sdfjhk sdjfhksdhf</p> 
+            <p>skdkfjlsdkjf sdfkjsdl sdlfkj dirn sldkfj</p> 
+            <p>KDSjflskjd  twefkljs kjliji nfdsnl. </p>    
+</div>
---
+++ header_section.php
@@ -0,0 +1,9 @@
+<?php $this->layout('template',['title' => 'header_section']);?>
+
+ <div class="container-fluid">
+ <div class="jumbotron jumbotron-fluid" style="background-color: blue; color: yellow;">
+     <div>
+         <h1>Track the Blue</h1>
+     </div>
+ </div>
+?>
